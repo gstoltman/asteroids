56b7a054882723e2f368ec6ace8c924feb49516c
gstoltman <gstoltman@gmail.com>
2025-03-29 20:32:04 -0700

    added fps fix

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -10,6 +12,8 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick() / 1000
 
 if __name__ == "__main__":
     main()
